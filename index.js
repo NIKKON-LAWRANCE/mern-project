1322aed929e284f99d10457d20253c2cdc716717
nikkon-roy <nikkonlawrance@gmail.com>
2022-08-01 10:56:35 +0600

    JSON data get and post

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app= express();
 const PORT = 3001;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // app.get("/userID/:id/userAge/:age",(req,res)=>{
 //     const id= req.params.id;
@@ -8,11 +12,16 @@ const PORT = 3001;
 //     res.send(`<h1>Student ID is ${id}, Age is ${age}</h1>`);
 // });
 
-app.get("/",(req,res)=>{
-    const id = req.header('id');
-    const name = req.header('name');
-    res.send(`<h1>Student ID is ${id}, name is ${name}</h1>`);
+// app.get("/",(req,res)=>{
+//     const id = req.header('id');
+//     const name = req.header('name');
+//     res.send(`<h1>Student ID is ${id}, name is ${name}</h1>`);
+// });
+app.post("/user",(req,res)=>{
+    const name= req.body.name;
+    res.send(`Welcome ${name}`);
 });
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is runing at http://localhost:${PORT}`);
